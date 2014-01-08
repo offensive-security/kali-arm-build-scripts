a13aebb1c485593be3fc2ade34049cf144d8a696
muts <muts@pro.local>
2014-01-08 15:55:08 -0400

    Added pixz as a dependency

--- build-deps.sh
+++ build-deps.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 apt-get install -y git-core gnupg flex bison gperf libesd0-dev build-essential \
 zip curl libncurses5-dev zlib1g-dev libncurses5-dev gcc-multilib g++-multilib \
-parted kpartx debootstrap qemu-user-static abootimg cgpt vboot-kernel-utils \
+parted kpartx debootstrap pixz qemu-user-static abootimg cgpt vboot-kernel-utils \
 vboot-utils uboot-mkimage bc lzma lzop build-essential automake autoconf m4 \
 schedtool
 MACHINE_TYPE=`uname -m`
