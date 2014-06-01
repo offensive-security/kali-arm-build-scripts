dc51cbef10856cb25efac11e5faeedcd003ff717
Steev Klimaszewski <steev@gentoo.org>
2014-06-01 18:33:42 -0500

    Add support for building RIoTboard images.
    This is a script to build images for the RIoTboard, which is a single core i.MX6 device from Embest/element14.
    
    There won't be official images released, but the script is here for people who want to try it out.  At the moment we build off a github kernel, but 3.16 should have support for the RIoTboard in mainline.

---
+++ riot.sh
@@ -0,0 +1,264 @@
+#!/bin/bash
+
+# This is the riot Kali ARM build script - http://www.riotboard.org
+# A trusted Kali Linux image created by Offensive Security - http://www.offensive-security.com
+
+if [[ $# -eq 0 ]] ; then
+    echo "Please pass version number, e.g. $0 1.0.1"
+    exit 0
+fi
+
+basedir=`pwd`/riot-$1
+
+# Make sure that the cross compiler can be found in the path before we do
+# anything else, that way the builds don't fail half way through.
+export CROSS_COMPILE=arm-linux-gnueabihf-
+if [ $(compgen -c $CROSS_COMPILE | wc -l) -eq 0 ] ; then
+    echo "Missing cross compiler. Set up PATH according to the README"
+    exit 1
+fi
+# Unset CROSS_COMPILE so that if there is any native compiling needed it doesn't
+# get cross compiled.
+unset CROSS_COMPILE
+
+# Package installations for various sections. 
+# This will build a minimal XFCE Kali system with the top 10 tools.
+# This is the section to edit if you would like to add more packages.
+# See http://www.kali.org/new/kali-linux-metapackages/ for meta packages you can
+# use. You can also install packages, using just the package name, but keep in
+# mind that not all packages work on ARM! If you specify one of those, the
+# script will throw an error, but will still continue on, and create an unusable
+# image, keep that in mind.
+
+arm="abootimg cgpt fake-hwclock ntpdate vboot-utils vboot-kernel-utils uboot-mkimage"
+base="kali-menu kali-defaults initramfs-tools usbutils"
+desktop="xfce4 network-manager network-manager-gnome xserver-xorg-video-fbdev"
+tools="passing-the-hash winexe aircrack-ng hydra john sqlmap wireshark libnfc-bin mfoc"
+services="openssh-server apache2"
+extras="iceweasel wpasupplicant"
+
+export packages="${arm} ${base} ${desktop} ${tools} ${services} ${extras}"
+export architecture="armhf"
+
+# Set this to use an http proxy, like apt-cacher-ng, and uncomment further down
+# to unset it.
+#export http_proxy="http://localhost:3142/"
+
+mkdir -p ${basedir}
+cd ${basedir}
+
+# create the rootfs - not much to modify here, except maybe the hostname.
+debootstrap --foreign --arch $architecture kali kali-$architecture http://http.kali.org/kali
+
+cp /usr/bin/qemu-arm-static kali-$architecture/usr/bin/
+
+LANG=C chroot kali-$architecture /debootstrap/debootstrap --second-stage
+cat << EOF > kali-$architecture/etc/apt/sources.list
+deb http://http.kali.org/kali kali main contrib non-free
+deb http://security.kali.org/kali-security kali/updates main contrib non-free
+EOF
+
+# Set hostname
+echo "kali" > kali-$architecture/etc/hostname
+
+cat << EOF > kali-$architecture/etc/hosts
+127.0.0.1       kali    localhost
+::1             localhost ip6-localhost ip6-loopback
+fe00::0         ip6-localnet
+ff00::0         ip6-mcastprefix
+ff02::1         ip6-allnodes
+ff02::2         ip6-allrouters
+EOF
+
+cat << EOF > kali-$architecture/etc/network/interfaces
+auto lo
+iface lo inet loopback
+
+auto eth0
+iface eth0 inet dhcp
+EOF
+
+cat << EOF > kali-$architecture/etc/resolv.conf
+nameserver 8.8.8.8
+EOF
+
+export MALLOC_CHECK_=0 # workaround for LP: #520465
+export LC_ALL=C
+export DEBIAN_FRONTEND=noninteractive
+
+mount -t proc proc kali-$architecture/proc
+mount -o bind /dev/ kali-$architecture/dev/
+mount -o bind /dev/pts kali-$architecture/dev/pts
+
+cat << EOF > kali-$architecture/debconf.set
+console-common console-data/keymap/policy select Select keymap from full list
+console-common console-data/keymap/full select en-latin1-nodeadkeys
+EOF
+
+cat << EOF > kali-$architecture/third-stage
+#!/bin/bash
+dpkg-divert --add --local --divert /usr/sbin/invoke-rc.d.chroot --rename /usr/sbin/invoke-rc.d
+cp /bin/true /usr/sbin/invoke-rc.d
+echo -e "#!/bin/sh\nexit 101" > /usr/sbin/policy-rc.d
+chmod +x /usr/sbin/policy-rc.d
+
+apt-get update
+apt-get install locales-all
+
+debconf-set-selections /debconf.set
+rm -f /debconf.set
+apt-get update
+apt-get -y install git-core binutils ca-certificates initramfs-tools uboot-mkimage
+apt-get -y install locales console-common less nano git
+echo "root:toor" | chpasswd
+sed -i -e 's/KERNEL\!=\"eth\*|/KERNEL\!=\"/' /lib/udev/rules.d/75-persistent-net-generator.rules
+rm -f /etc/udev/rules.d/70-persistent-net.rules
+apt-get --yes --force-yes install $packages
+
+rm -f /usr/sbin/policy-rc.d
+rm -f /usr/sbin/invoke-rc.d
+dpkg-divert --remove --rename /usr/sbin/invoke-rc.d
+
+rm -f /third-stage
+EOF
+
+chmod +x kali-$architecture/third-stage
+LANG=C chroot kali-$architecture /third-stage
+
+cat << EOF > kali-$architecture/cleanup
+#!/bin/bash
+rm -rf /root/.bash_history
+apt-get update
+apt-get clean
+rm -f /0
+rm -f /hs_err*
+rm -f cleanup
+rm -f /usr/bin/qemu*
+EOF
+
+chmod +x kali-$architecture/cleanup
+LANG=C chroot kali-$architecture /cleanup
+
+umount kali-$architecture/proc/sys/fs/binfmt_misc
+umount kali-$architecture/dev/pts
+umount kali-$architecture/dev/
+umount kali-$architecture/proc
+
+# Create the disk and partition it
+echo "Creating image file for riot"
+dd if=/dev/zero of=${basedir}/kali-$1-riot.img bs=1M count=7000
+parted kali-$1-riot.img --script -- mklabel msdos
+parted kali-$1-riot.img --script -- mkpart primary fat32 2048s 264191s
+parted kali-$1-riot.img --script -- mkpart primary ext4 264192s 100%
+
+# Set the partition variables
+loopdevice=`losetup -f --show ${basedir}/kali-$1-riot.img`
+device=`kpartx -va $loopdevice| sed -E 's/.*(loop[0-9])p.*/\1/g' | head -1`
+device="/dev/mapper/${device}"
+bootp=${device}p1
+rootp=${device}p2
+
+# Create file systems
+mkfs.vfat $bootp
+mkfs.ext4 $rootp
+
+# Create the dirs for the partitions and mount them
+mkdir -p ${basedir}/bootp ${basedir}/root
+mount $bootp ${basedir}/bootp
+mount $rootp ${basedir}/root
+
+echo "Rsyncing rootfs into image file"
+rsync -HPavz -q ${basedir}/kali-$architecture/ ${basedir}/root/
+
+# Enable serial console access
+echo "T1:23:respawn:/sbin/agetty -L ttymxc1 115200 vt100" >> ${basedir}/root/etc/inittab
+
+cat << EOF >> ${basedir}/root/etc/udev/links.conf
+M   ttymxc1 c   5 1
+EOF
+
+cat << EOF >> ${basedir}/root/etc/securetty
+ttymxc1
+EOF
+
+# Uncomment this if you use apt-cacher-ng otherwise git clones will fail
+#unset http_proxy
+
+# Kernel section. If you want to use a custom kernel, or configuration, replace
+# them in this section.
+#
+# Mainline-ish - 3.15.0-rc7 base with some i.MX and RIoTboard fixes on top.
+git clone --depth 1 -b riotboard-dts4 git://github.com/selsinork/linux.git ${basedir}/kernel
+cd ${basedir}/kernel
+mkdir -p ../patches
+wget http://patches.aircrack-ng.org/mac80211.compat08082009.wl_frag+ack_v1.patch -O ../patches/mac80211.patch
+patch -p1 --no-backup-if-mismatch < ../patches/mac80211.patch
+touch .scmversion
+export ARCH=arm
+export CROSS_COMPILE=arm-linux-gnueabihf-
+cp ${basedir}/../kernel-configs/riot.config
+make -j $(grep -c processor /proc/cpuinfo)
+make modules_install INSTALL_MOD_PATH=${basedir}/root
+cp arch/arm/boot/zImage ${basedir}/bootp/
+cp arch/arm/boot/dts/imx6dl-riotboard.dtb ${basedir}/bootp/
+cd ${basedir}
+
+rm -rf ${basedir}/root/lib/firmware
+cd ${basedir}/root/lib
+git clone --depth 1 https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git firmware
+rm -rf ${basedir}/root/lib/firmware/.git
+# Need some firmware from the kernel so..
+cd ${basedir}/kernel
+make modules_install INSTALL_MOD_PATH=${basedir}/root
+cd ${basedir}
+
+# Mainline u-boot with RIoTboard fixes on top
+git clone -b embest-boards-fixes --depth 1 git://github.com/selsinork/u-boot.git
+cd u-boot
+make riotboard_config
+make -j $(grep -c processor /proc/cpuinfo)
+dd if=u-boot.imx of=$loopdevice bs=1024 seek=1
+cd ${basedir}
+
+# Generate the bootscript so that u-boot knows where everything is...
+cat << __EOF__ > ${basedir}/bootp/bootscript
+fdt_high=0xffffffff
+initrd_high=0xffffffff
+
+kaddr=0x12000000
+
+loadkernel=load \${dtype} \${disk}:1 \${loadaddr} zImage
+
+bargs=setenv bootargs console=ttymxc1,115200n8 rootwait root=PARTUUID=\${btpart}
+
+loadfdt=load \${dtype} \${disk}:1 0x11000000 \${fdt_file}
+
+doboot=part uuid \${dtype} \${disk}:2 btpart ; run bargs; if run loadkernel; then echo kernel_loaded ; if run loadfdt; then echo fdt_loaded; bootz \${loadaddr} - 0x11000000 ; else echo fail1 ; fi ; fi ; echo failed to boot
+__EOF__
+
+# Unmount partitions
+umount $bootp
+umount $rootp
+kpartx -dv $loopdevice
+losetup -d $loopdevice
+
+# Clean up all the temporary build stuff and remove the directories.
+# Comment this out to keep things around if you want to see what may have gone
+# wrong.
+echo "Removing temporary build files"
+rm -rf ${basedir}/kernel ${basedir}/bootp ${basedir}/root ${basedir}/kali-$architecture ${basedir}/boot ${basedir}/u-boot ${basedir}/patches
+
+# If you're building an image for yourself, comment all of this out, as you
+# don't need the sha1sum or to compress the image, since you will be testing it
+# soon.
+echo "Generating sha1sum for kali-$1-riot.img"
+sha1sum kali-$1-riot.img > ${basedir}/kali-$1-riot.img.sha1sum
+# Don't pixz on 32bit, there isn't enough memory to compress the images.
+MACHINE_TYPE=`uname -m`
+if [ ${MACHINE_TYPE} == 'x86_64' ]; then
+echo "Compressing kali-$1-riot.img"
+pixz ${basedir}/kali-$1-riot.img ${basedir}/kali-$1-riot.img.xz
+rm ${basedir}/kali-$1-riot.img
+echo "Generating sha1sum for kali-$1-riot.img.xz"
+sha1sum kali-$1-riot.img.xz > ${basedir}/kali-$1-riot.img.xz.sha1sum
+fi
