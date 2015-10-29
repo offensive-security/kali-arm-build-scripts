4fa39b23f5a9cf2016c2bdd757bb076821b77287
Philippe Morin <pmorin@live.ca>
2015-10-28 22:32:03 -0400

    Fix for CubieTruck boot.
    Fix from MAJORUBOOTFUQUP for CubieTruck tested and working.

--- cubietruck.sh
+++ cubietruck.sh
@@ -222,6 +222,7 @@ cd ${basedir}
 
 # Create boot.txt file
 cat << EOF > ${basedir}/bootp/boot.cmd
+setenv bootm_boot_mode sec
 setenv bootargs console=ttyS0,115200 root=/dev/mmcblk0p2 rootwait panic=10 ${extra}
 fatload mmc 0 0x43000000 script.bin
 fatload mmc 0 0x48000000 uImage
