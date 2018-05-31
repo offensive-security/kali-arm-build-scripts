11c4596f935d262407ebd4758cf794f7d9ab6da6
root <root@localhost.localdomain>
2018-05-31 16:55:42 +0000

    updated brfs-tools to brfs-progs

--- build-deps.sh
+++ build-deps.sh
@@ -3,7 +3,7 @@ apt-get install -y git-core gnupg flex bison gperf build-essential \
 zip curl libncurses5-dev zlib1g-dev libncurses5-dev gcc-multilib g++-multilib \
 parted kpartx debootstrap pixz qemu-user-static abootimg cgpt vboot-kernel-utils \
 vboot-utils u-boot-tools bc lzma lzop automake autoconf m4 dosfstools rsync \
-schedtool git dosfstools e2fsprogs device-tree-compiler libssl-dev qemu-user-static btrfs-tools
+schedtool git dosfstools e2fsprogs device-tree-compiler libssl-dev qemu-user-static btrfs-progs
 MACHINE_TYPE=`uname -m`
 if [ ${MACHINE_TYPE} == 'x86_64' ]; then
     dpkg --add-architecture i386
