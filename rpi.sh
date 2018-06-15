d0416250eea32cf2a8052b5b4e42befba71bd094
mutsio <40067008+mutsio@users.noreply.github.com>
2018-06-15 11:50:48 -0400

    More aggressive exits for the automated build

--- rpi.sh
+++ rpi.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+set -e
 # This is the Raspberry Pi Kali ARM build script - http://www.kali.org/downloads
 # A trusted Kali Linux image created by Offensive Security - http://www.offensive-security.com
 
