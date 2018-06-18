e8dacd0aa4c83f5713eea0621fd7c27451af92d4
Steev Klimaszewski <steev@kali.org>
2018-06-18 08:25:17 -0500

    Update build-deps
    We need the crossbuild-essential-* packages to build for the respective
    architectures.  We no longer do anything with btrfs so remove installing
    the tools.  Also ensure we apt-get update after adding the i386
    architecture so we can run x86 binaries on amd64

--- build-deps.sh
+++ build-deps.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 apt-get update
 apt-get install -y git-core gnupg flex bison gperf build-essential \
-zip curl libncurses5-dev zlib1g-dev libncurses5-dev gcc-multilib g++-multilib \
+zip curl libncurses5-dev zlib1g-dev libncurses5-dev \
 parted kpartx debootstrap pixz qemu-user-static abootimg cgpt vboot-kernel-utils \
 vboot-utils u-boot-tools bc lzma lzop automake autoconf m4 dosfstools rsync \
 schedtool git dosfstools e2fsprogs device-tree-compiler libssl-dev qemu-user-static \
-btrfs-tools 
-
-apt-get install -y crossbuild-essential-armhf
+crossbuild-essential-armhf crossbuild-essential-armel crossbuild-essential-arm64
 
 MACHINE_TYPE=`uname -m`
 if [ ${MACHINE_TYPE} == 'x86_64' ]; then
     dpkg --add-architecture i386
+    # apt-get update to make sure we have a list of the i386 packages
+    apt-get update
     apt-get install -y libstdc++6:i386 libgcc1:i386 zlib1g:i386 libncurses5:i386
 else
     apt-get install -y libncurses5
