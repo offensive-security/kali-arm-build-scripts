66fb9d7591c98289574e829496c44339cb9a36dc
Steev Klimaszewski <steev@kali.org>
2018-06-18 12:12:46 -0500

    Update odroid-c2 image
    Update the kernel config, crypto extensions don't seem to build
    correctly with gcc 7.3 when cross compiling.  Also pass the path to the
    kali keyring on the debootstrap line.

--- odroid-c2.sh
+++ odroid-c2.sh
@@ -59,7 +59,7 @@ mkdir -p ${basedir}
 cd ${basedir}
 
 # create the rootfs - not much to modify here, except maybe the hostname.
-debootstrap --foreign --arch $architecture kali-rolling kali-$architecture http://$mirror/kali
+debootstrap --foreign --keyring=/usr/share/keyrings/kali-archive-keyring.gpg --arch $architecture kali-rolling kali-$architecture http://$mirror/kali
 
 cp /usr/bin/qemu-aarch64-static kali-$architecture/usr/bin/
 cp /usr/bin/qemu-arm-static kali-$architecture/usr/bin/
