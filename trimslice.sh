36b7d764ca024d3519eb3babf9cb7d736557911c
Steev Klimaszewski <steev@kali.org>
2018-06-22 20:45:11 -0500

    Updated (non-working) trimslice script
    Adding the trimslice script - it'll build an image, but the image won't
    boot - we're moving the trimslice and riot to the kali kernels instead
    of custom vendor kernels, and unfortunately, flash-kernel doesn't have a
    way to detect being run under qemu so it immediately exits.  Need to go
    through flash-kernel and pull out the important bits to create the
    initial uboot environment, and then create a service that will install
    flash-kernel onto the system.  The only reason it fails is due to the
    image not being created on a trimslice.

--- trimslice.sh
+++ trimslice.sh
@@ -3,6 +3,11 @@
 # This is the Trimslice Kali ARM build script - http://utilite-computer.com/web/home
 # A trusted Kali Linux image created by Offensive Security - http://www.offensive-security.com
 
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root"
+   exit 1
+fi
+
 if [[ $# -eq 0 ]] ; then
     echo "Please pass version number, e.g. $0 2.0"
     exit 0
@@ -31,7 +36,7 @@ unset CROSS_COMPILE
 # image, keep that in mind.
 
 arm="abootimg cgpt fake-hwclock ntpdate u-boot-tools vboot-utils vboot-kernel-utils"
-base="e2fsprogs initramfs-tools kali-defaults kali-menu parted sudo usbutils"
+base="e2fsprogs initramfs-tools kali-defaults kali-menu parted sudo usbutils firmware-linux firmware-atheros firmware-libertas firmware-realtek linux-image-armmp"
 desktop="fonts-croscore fonts-crosextra-caladea fonts-crosextra-carlito gnome-theme-kali gtk3-engines-xfce kali-desktop-xfce kali-root-login lightdm network-manager network-manager-gnome xfce4 xserver-xorg-video-fbdev"
 tools="aircrack-ng ethtool hydra john libnfc-bin mfoc nmap passing-the-hash sqlmap usbutils winexe wireshark"
 services="apache2 openssh-server"
@@ -97,12 +102,41 @@ console-common console-data/keymap/policy select Select keymap from full list
 console-common console-data/keymap/full select en-latin1-nodeadkeys
 EOF
 
+cat << 'EOF' > kali-$architecture/lib/systemd/system/regenerate_ssh_host_keys.service
+[Unit]
+Description=Regenerate SSH host keys
+Before=ssh.service
+[Service]
+Type=oneshot
+ExecStartPre=-/bin/dd if=/dev/hwrng of=/dev/urandom count=1 bs=4096
+ExecStartPre=-/bin/sh -c "/bin/rm -f -v /etc/ssh/ssh_host_*_key*"
+ExecStart=/usr/bin/ssh-keygen -A -v
+ExecStartPost=/bin/sh -c "for i in /etc/ssh/ssh_host_*_key*; do actualsize=$(wc -c <\"$i\") ;if [ $actualsize -eq 0 ]; then echo size is 0 bytes ; exit 1 ; fi ; done ; /bin/systemctl disable regenerate_ssh_host_keys"
+[Install]
+WantedBy=multi-user.target
+EOF
+chmod 644 kali-$architecture/lib/systemd/system/regenerate_ssh_host_keys.service
+
+cat << EOF > kali-$architecture/lib/systemd/system/rpiwiggle.service
+[Unit]
+Description=Resize filesystem
+Before=regenerate_ssh_host_keys.service
+[Service]
+Type=oneshot
+ExecStart=/root/scripts/rpi-wiggle.sh
+ExecStartPost=/bin/systemctl disable rpiwiggle
+ExecStartPost=/sbin/reboot
+[Install]
+WantedBy=multi-user.target
+EOF
+chmod 644 kali-$architecture/lib/systemd/system/rpiwiggle.service
+
 cat << EOF > kali-$architecture/third-stage
 #!/bin/bash
 dpkg-divert --add --local --divert /usr/sbin/invoke-rc.d.chroot --rename /usr/sbin/invoke-rc.d
 cp /bin/true /usr/sbin/invoke-rc.d
 echo -e "#!/bin/sh\nexit 101" > /usr/sbin/policy-rc.d
-chmod +x /usr/sbin/policy-rc.d
+chmod 755 /usr/sbin/policy-rc.d
 
 apt-get update
 apt-get --yes --allow-change-held-packages install locales-all
@@ -113,7 +147,6 @@ apt-get update
 apt-get -y install git-core binutils ca-certificates initramfs-tools u-boot-tools
 apt-get -y install locales console-common less nano git
 echo "root:toor" | chpasswd
-sed -i -e 's/KERNEL\!=\"eth\*|/KERNEL\!=\"/' /lib/udev/rules.d/75-persistent-net-generator.rules
 rm -f /etc/udev/rules.d/70-persistent-net.rules
 export DEBIAN_FRONTEND=noninteractive
 apt-get --yes --allow-change-held-packages install $packages
@@ -124,6 +157,13 @@ fi
 apt-get --yes --allow-change-held-packages dist-upgrade
 apt-get --yes --allow-change-held-packages autoremove
 
+# Resize FS on first run (hopefully)
+systemctl enable rpiwiggle
+
+# Generate SSH host keys on first run
+systemctl enable regenerate_ssh_host_keys
+systemctl enable ssh
+
 rm -f /usr/sbin/policy-rc.d
 rm -f /usr/sbin/invoke-rc.d
 dpkg-divert --remove --rename /usr/sbin/invoke-rc.d
@@ -131,7 +171,7 @@ dpkg-divert --remove --rename /usr/sbin/invoke-rc.d
 rm -f /third-stage
 EOF
 
-chmod +x kali-$architecture/third-stage
+chmod 755 kali-$architecture/third-stage
 LANG=C systemd-nspawn -M trimslice -D kali-$architecture /third-stage
 
 cat << EOF > kali-$architecture/cleanup
@@ -145,7 +185,7 @@ rm -f cleanup
 rm -f /usr/bin/qemu*
 EOF
 
-chmod +x kali-$architecture/cleanup
+chmod 755 kali-$architecture/cleanup
 LANG=C systemd-nspawn -M trimslice -D kali-$architecture /cleanup
 
 #umount kali-$architecture/proc/sys/fs/binfmt_misc
@@ -201,80 +241,137 @@ EOF
 
 # Kernel section. If you want to use a custom kernel, or configuration, replace
 # them in this section.
-git clone --depth 1 git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git -b linux-4.1.y ${basedir}/root/usr/src/kernel
-cd ${basedir}/root/usr/src/kernel
-git rev-parse HEAD > ../kernel-at-commit
-patch -p1 --no-backup-if-mismatch < ${basedir}/../patches/mac80211.patch
-patch -p1 --no-backup-if-mismatch < ${basedir}/../patches/0001-wireless-carl9170-Enable-sniffer-mode-promisc-flag-t.patch
-touch .scmversion
-export ARCH=arm
-export CROSS_COMPILE=arm-linux-gnueabihf-
-cp ${basedir}/../kernel-configs/trimslice.config .config
-cp ${basedir}/../kernel-configs/trimslice.config ../trimslice.config
-make -j $(grep -c processor /proc/cpuinfo) zImage modules dtbs
-make modules_install INSTALL_MOD_PATH=${basedir}/root
-cp arch/arm/boot/zImage ${basedir}/bootp/
-cp arch/arm/boot/dts/tegra20-trimslice.dtb ${basedir}/bootp/
-make mrproper
-cp ../trimslice.config .config
-make modules_prepare
-cd ${basedir}
+#git clone --depth 1 git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git -b linux-4.1.y ${basedir}/root/usr/src/kernel
+#cd ${basedir}/root/usr/src/kernel
+#git rev-parse HEAD > ../kernel-at-commit
+#patch -p1 --no-backup-if-mismatch < ${basedir}/../patches/mac80211.patch
+#patch -p1 --no-backup-if-mismatch < ${basedir}/../patches/0001-wireless-carl9170-Enable-sniffer-mode-promisc-flag-t.patch
+#touch .scmversion
+#export ARCH=arm
+#export CROSS_COMPILE=arm-linux-gnueabihf-
+#cp ${basedir}/../kernel-configs/trimslice.config .config
+#cp ${basedir}/../kernel-configs/trimslice.config ../trimslice.config
+#make -j $(grep -c processor /proc/cpuinfo) zImage modules dtbs
+#make modules_install INSTALL_MOD_PATH=${basedir}/root
+#cp arch/arm/boot/zImage ${basedir}/bootp/
+#cp arch/arm/boot/dts/tegra20-trimslice.dtb ${basedir}/bootp/
+#make mrproper
+#cp ../trimslice.config .config
+#make modules_prepare
+#cd ${basedir}
 
 # Fix up the symlink for building external modules
 # kernver is used so we don't need to keep track of what the current compiled
 # version is
-kernver=$(ls ${basedir}/root/lib/modules/)
-cd ${basedir}/root/lib/modules/$kernver
-rm build
-rm source
-ln -s /usr/src/kernel build
-ln -s /usr/src/kernel source
+#kernver=$(ls ${basedir}/root/lib/modules/)
+#cd ${basedir}/root/lib/modules/$kernver
+#rm build
+#rm source
+#ln -s /usr/src/kernel build
+#ln -s /usr/src/kernel source
 cd ${basedir}
 
-rm -rf ${basedir}/root/lib/firmware
-cd ${basedir}/root/lib
-git clone --depth 1 https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git firmware
-rm -rf ${basedir}/root/lib/firmware/.git
-cd ${basedir}
+#rm -rf ${basedir}/root/lib/firmware
+#cd ${basedir}/root/lib
+#git clone --depth 1 https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git firmware
+#rm -rf ${basedir}/root/lib/firmware/.git
+#cd ${basedir}
 
-echo << EOF > ${basedir}/bootp/boot.txt
-setenv bootargs root=/dev/mmcblk0p2 nohdparm rootwait console=ttyS0,115200n8 earlyprintk net.ifnames=0
-ext2load usb 0:1 4080000 zImage
-ext2load usb 0:1 4000000 tegra20-trimslice.dtb
-bootz 4080000 - 4000000
+#echo << EOF > ${basedir}/bootp/boot.txt
+#setenv bootargs root=/dev/mmcblk0p2 nohdparm rootwait console=ttyS0,115200n8 earlyprintk net.ifnames=0
+#ext2load usb 0:1 4080000 zImage
+#ext2load usb 0:1 4000000 tegra20-trimslice.dtb
+#bootz 4080000 - 4000000
+#EOF
+
+cat << EOF > ${basedir}/bootp/boot.txt
+# Bootscript using the new unified bootcmd handling
+# introduced with u-boot v2014.10
+#
+# Expects to be called with the following environment variables set:
+#
+#  devtype              e.g. mmc/scsi etc
+#  devnum               The device number of the given type
+#  bootpart             The partition containing the boot files
+#  distro_bootpart      The partition containing the boot files
+#                       (introduced in u-boot mainline 2016.01)
+#  prefix               Prefix within the boot partiion to the boot files
+#  kernel_addr_r        Address to load the kernel to
+#  fdt_addr_r           Address to load the FDT to
+#  ramdisk_addr_r       Address to load the initrd to.
+#
+# The uboot must support the bootz and generic filesystem load commands.
+
+# Workaround lack of baudrate included with console on various iMX
+# systems (e.g. wandboard, cubox-i, hummingboard)
+if test "\${console}" = "ttymxc0" && test -n "\${baudrate}"; then
+  setenv console "\${console},\${baudrate}"
+fi
+
+if test -n "\${console}"; then
+  setenv bootargs "\${bootargs} console=\${console}"
+fi
+
+setenv bootargs @@LINUX_KERNEL_CMDLINE_DEFAULTS@@ \${bootargs} @@LINUX_KERNEL_CMDLINE@@
+@@UBOOT_ENV_EXTRA@@
+
+if test -z "\${fk_kvers}"; then
+   setenv fk_kvers '@@KERNEL_VERSION@@'
+fi
+
+# These two blocks should be the same apart from the use of
+# \${fk_kvers} in the first, the syntax supported by u-boot does not
+# lend itself to removing this duplication.
+
+if test -n "\${fdtfile}"; then
+   setenv fdtpath dtbs/\${fk_kvers}/\${fdtfile}
+else
+   setenv fdtpath dtb-\${fk_kvers}
+fi
+
+if test -z "\${distro_bootpart}"; then
+  setenv partition \${bootpart}
+else
+  setenv partition \${distro_bootpart}
+fi
+
+load \${devtype} \${devnum}:\${partition} \${kernel_addr_r} \${prefix}vmlinuz-\${fk_kvers} \
+&& load \${devtype} \${devnum}:\${partition} \${fdt_addr_r} \${prefix}\${fdtpath} \
+&& load \${devtype} \${devnum}:\${partition} \${ramdisk_addr_r} \${prefix}initrd.img-\${fk_kvers} \
+&& echo "Booting Kali \${fk_kvers} from \${devtype} \${devnum}:\${partition}..." \
+&& bootz \${kernel_addr_r} \${ramdisk_addr_r}:\${filesize} \${fdt_addr_r}
+
+load \${devtype} \${devnum}:\${partition} \${kernel_addr_r} \${prefix}vmlinuz \
+&& load \${devtype} \${devnum}:\${partition} \${fdt_addr_r} \${prefix}dtb \
+&& load \${devtype} \${devnum}:\${partition} \${ramdisk_addr_r} \${prefix}initrd.img \
+&& echo "Booting Kali from \${devtype} \${devnum}:\${partition}..." \
+&& bootz \${kernel_addr_r} \${ramdisk_addr_r}:\${filesize} \${fdt_addr_r}
 EOF
 
 # Create u-boot boot script image
 mkimage -A arm -T script -C none -d ${basedir}/bootp/boot.txt ${basedir}/bootp/boot.scr
 
 cp ${basedir}/../misc/zram ${basedir}/root/etc/init.d/zram
-chmod +x ${basedir}/root/etc/init.d/zram
+chmod 755 ${basedir}/root/etc/init.d/zram
 
 sed -i -e 's/^#PermitRootLogin.*/PermitRootLogin yes/' ${basedir}/root/etc/ssh/sshd_config
 
 # Unmount partitions
-umount $bootp
-umount $rootp
+sync
+umount -l $bootp
+umount -l $rootp
 kpartx -dv $loopdevice
 losetup -d $loopdevice
 
-# Clean up all the temporary build stuff and remove the directories.
-# Comment this out to keep things around if you want to see what may have gone
-# wrong.
-echo "Removing temporary build files"
-rm -rf ${basedir}/patches ${basedir}/bootp ${basedir}/root ${basedir}/kali-$architecture ${basedir}/boot
-
-# If you're building an image for yourself, comment all of this out, as you
-# don't need the sha256sum or to compress the image, since you will be testing it
-# soon.
-echo "Generating sha256sum for kali-linux-$1-trimslice.img"
-sha256sum kali-linux-$1-trimslice.img > ${basedir}/kali-linux-$1-trimslice.img.sha256sum
 # Don't pixz on 32bit, there isn't enough memory to compress the images.
 MACHINE_TYPE=`uname -m`
 if [ ${MACHINE_TYPE} == 'x86_64' ]; then
 echo "Compressing kali-linux-$1-trimslice.img"
-pixz ${basedir}/kali-linux-$1-trimslice.img ${basedir}/kali-linux-$1-trimslice.img.xz
+pixz ${basedir}/kali-linux-$1-trimslice.img ${basedir}/../kali-linux-$1-trimslice.img.xz
 rm ${basedir}/kali-linux-$1-trimslice.img
-echo "Generating sha256sum for kali-linux-$1-trimslice.img.xz"
-sha256sum kali-linux-$1-trimslice.img.xz > ${basedir}/kali-linux-$1-trimslice.img.xz.sha256sum
 fi
+
+# Clean up all the temporary build stuff and remove the directories.
+# Comment this out to keep things around if you want to see what may have gone
+# wrong.
+rm -rf ${basedir}
