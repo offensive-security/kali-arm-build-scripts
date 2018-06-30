5f8b89b9e314f42f1fbfd6d005263aa8eba34c69
Steev Klimaszewski <steev@kali.org>
2018-06-30 01:34:20 -0500

    Update the veyron script to latest changes as well.

--- chromebook-arm-veyron.sh
+++ chromebook-arm-veyron.sh
@@ -13,9 +13,14 @@ fi
 basedir=`pwd`/veyron-$1
 
 hostname=kali
+imagename=kali-linux-$1-veyron
 
 if [ $2 ]; then
     hostname=$2
+fi
+
+if [ $3 ]; then
+    imagename=$3
 fi
 
 # Generate a random machine name to be used.
@@ -33,7 +38,7 @@ fi
 unset CROSS_COMPILE
 
 # Package installations for various sections.
-# This will build a minimal Gnome Kali system with the top 10 tools.
+# This will build a minimal Kali system with XFCE and a few tools.
 # This is the section to edit if you would like to add more packages.
 # See http://www.kali.org/new/kali-linux-metapackages/ for meta packages you can
 # use.  You can also install packages, using just the package name, but keep in
@@ -42,11 +47,11 @@ unset CROSS_COMPILE
 # image, keep that in mind.
 
 arm="abootimg cgpt fake-hwclock ntpdate u-boot-tools vboot-utils vboot-kernel-utils"
-base="alsa-utils e2fsprogs initramfs-tools kali-defaults kali-menu parted pulseaudio sudo usbutils"
+base="alsa-utils e2fsprogs initramfs-tools kali-defaults kali-menu parted pulseaudio sudo usbutils firmware-linux firmware-atheros firmware-libertas firmware-realtek"
 desktop="fonts-croscore fonts-crosextra-caladea fonts-crosextra-carlito gnome-theme-kali gtk3-engines-xfce kali-desktop-xfce kali-root-login lightdm network-manager network-manager-gnome xfce4 xserver-xorg-video-fbdev xserver-xorg-input-synaptics xserver-xorg-input-all xserver-xorg-input-libinput"
 tools="aircrack-ng ethtool hydra john libnfc-bin mfoc nmap passing-the-hash sqlmap usbutils winexe wireshark"
 services="apache2 openssh-server"
-extras="florence iceweasel xfce4-goodies xfce4-terminal xinput wpasupplicant firmware-linux firmware-linux-nonfree firmware-libertas firmware-atheros"
+extras="florence iceweasel xfce4-goodies xfce4-terminal xinput wpasupplicant"
 
 packages="${arm} ${base} ${desktop} ${tools} ${services} ${extras}"
 architecture="armhf"
@@ -124,7 +129,6 @@ apt-get update
 apt-get -y install git-core binutils ca-certificates initramfs-tools u-boot-tools
 apt-get -y install locales console-common less nano git
 echo "root:toor" | chpasswd
-sed -i -e 's/KERNEL\!=\"eth\*|/KERNEL\!=\"/' /lib/udev/rules.d/75-persistent-net-generator.rules
 rm -f /etc/udev/rules.d/70-persistent-net.rules
 export DEBIAN_FRONTEND=noninteractive
 apt-get --yes --allow-change-held-packages install $packages
@@ -168,31 +172,7 @@ EOF
 #umount kali-$architecture/dev/
 #umount kali-$architecture/proc
 
-echo "Creating image file for Veyron Chromebooks"
-dd if=/dev/zero of=${basedir}/kali-linux-$1-veyron.img bs=1M count=7000
-parted kali-linux-$1-veyron.img --script -- mklabel gpt
-cgpt create -z kali-linux-$1-veyron.img
-cgpt create kali-linux-$1-veyron.img
-
-cgpt add -i 1 -t kernel -b 8192 -s 32768 -l kernel -S 1 -T 5 -P 10 kali-linux-$1-veyron.img
-cgpt add -i 2 -t data -b 40960 -s `expr $(cgpt show kali-linux-$1-veyron.img | grep 'Sec GPT table' | awk '{ print \$1 }')  - 40960` -l Root kali-linux-$1-veyron.img
-
-loopdevice=`losetup -f --show ${basedir}/kali-linux-$1-veyron.img`
-device=`kpartx -va $loopdevice| sed -E 's/.*(loop[0-9])p.*/\1/g' | head -1`
-sleep 5
-device="/dev/mapper/${device}"
-bootp=${device}p1
-rootp=${device}p2
-
-mkfs.ext4 -O ^flex_bg -O ^metadata_csum -L rootfs $rootp
-
-mkdir -p ${basedir}/root
-mount $rootp ${basedir}/root
-
-echo "Rsyncing rootfs into image file"
-rsync -HPavz -q ${basedir}/kali-$architecture/ ${basedir}/root/
-
-cat << EOF > ${basedir}/root/etc/apt/sources.list
+cat << EOF > ${basedir}/kali-$architecture/etc/apt/sources.list
 deb http://http.kali.org/kali kali-rolling main contrib non-free
 deb-src http://http.kali.org/kali kali-rolling main contrib non-free
 EOF
@@ -202,13 +182,14 @@ EOF
 
 # Kernel section.  If you want to use a custom kernel, or configuration, replace
 # them in this section.
-git clone --depth 1 https://chromium.googlesource.com/chromiumos/third_party/kernel -b chromeos-3.14 ${basedir}/root/usr/src/kernel
-cd ${basedir}/root/usr/src/kernel
+git clone --depth 1 https://chromium.googlesource.com/chromiumos/third_party/kernel -b chromeos-3.14 ${basedir}/kali-$architecture/usr/src/kernel
+cd ${basedir}/kali-$architecture/usr/src/kernel
 cp ${basedir}/../kernel-configs/chromebook-3.14_wireless-3.8.config .config
 cp ${basedir}/../kernel-configs/chromebook-3.14_wireless-3.8.config ../veyron.config
 export ARCH=arm
 # Edit the CROSS_COMPILE variable as needed.
 export CROSS_COMPILE=arm-linux-gnueabihf-
+# This allows us to patch the kernel without it adding -dirty to the kernel version.
 touch .scmversion
 patch -p1 --no-backup-if-mismatch < ${basedir}/../patches/mac80211-3.8.patch
 patch -p1 --no-backup-if-mismatch < ${basedir}/../patches/0002-mwifiex-do-not-create-AP-and-P2P-interfaces-upon-dri.patch
@@ -222,8 +203,8 @@ patch -p1 --no-backup-if-mismatch < ${basedir}/../patches/3711edaf01a01818f2aed9
 patch -p1 --no-backup-if-mismatch < ${basedir}/../patches/615829a03dc729e78372d40d95ba40e2ad51783b.patch
 make WIFIVERSION="-3.8" -j$(grep -c processor /proc/cpuinfo)
 make WIFIVERSION="-3.8" dtbs
-make WIFIVERSION="-3.8" modules_install INSTALL_MOD_PATH=${basedir}/root
-cat << __EOF__ > ${basedir}/root/usr/src/kernel/arch/arm/boot/kernel-veyron.its
+make WIFIVERSION="-3.8" modules_install INSTALL_MOD_PATH=${basedir}/kali-$architecture
+cat << __EOF__ > ${basedir}/kali-$architecture/usr/src/kernel/arch/arm/boot/kernel-veyron.its
 /dts-v1/;
  
 / {
@@ -540,7 +521,7 @@ cat << __EOF__ > ${basedir}/root/usr/src/kernel/arch/arm/boot/kernel-veyron.its
     };
 };
 __EOF__
-cd ${basedir}/root/usr/src/kernel/arch/arm/boot
+cd ${basedir}/kali-$architecture/usr/src/kernel/arch/arm/boot
 mkimage -D "-I dts -O dtb -p 2048" -f kernel-veyron.its veyron-kernel
 
 # BEHOLD THE MAGIC OF PARTUUID/PARTNROFF
@@ -551,7 +532,7 @@ echo 'noinitrd console=tty1 quiet root=PARTUUID=%U/PARTNROFF=1 rootwait rw lsm.m
 dd if=/dev/zero of=bootloader.bin bs=512 count=1
 
 vbutil_kernel --arch arm --pack ${basedir}/kernel.bin --keyblock /usr/share/vboot/devkeys/kernel.keyblock --signprivate /usr/share/vboot/devkeys/kernel_data_key.vbprivk --version 1 --config cmdline --bootloader bootloader.bin --vmlinuz veyron-kernel
-cd ${basedir}/root/usr/src/kernel
+cd ${basedir}/kali-$architecture/usr/src/kernel
 make WIFIVERSION="-3.8" mrproper
 cp ${basedir}/../kernel-configs/chromebook-3.14_wireless-3.8.config .config
 make WIFIVERSION="-3.8" modules_prepare
@@ -560,8 +541,8 @@ cd ${basedir}
 # Fix up the symlink for building external modules
 # kernver is used so we don't need to keep track of what the current compiled
 # version is
-kernver=$(ls ${basedir}/root/lib/modules/)
-cd ${basedir}/root/lib/modules/$kernver
+kernver=$(ls ${basedir}/kali-$architecture/lib/modules/)
+cd ${basedir}/kali-$architecture/lib/modules/$kernver
 rm build
 rm source
 ln -s /usr/src/kernel build
@@ -569,16 +550,16 @@ ln -s /usr/src/kernel source
 cd ${basedir}
 
 # Bit of a hack to hide the emmc partitions from XFCE
-cat << EOF > ${basedir}/root/etc/udev/rules.d/99-hide-emmc-partitions.rules
+cat << EOF > ${basedir}/kali-$architecture/etc/udev/rules.d/99-hide-emmc-partitions.rules
 KERNEL=="mmcblk0*", ENV{UDISKS_IGNORE}="1"
 EOF
 
 # Disable uap0 and p2p0 interfaces in NetworkManager
-printf '\n[keyfile]\nunmanaged-devices=interface-name:p2p0\n' >> ${basedir}/root/etc/NetworkManager/NetworkManager.conf
+printf '\n[keyfile]\nunmanaged-devices=interface-name:p2p0\n' >> ${basedir}/kali-$architecture/etc/NetworkManager/NetworkManager.conf
 
 # Create these if they don't exist, to make sure we have proper audio with pulse
-mkdir -p ${basedir}/root/var/lib/alsa/
-cat << EOF > ${basedir}/root/var/lib/alsa/asound.state
+mkdir -p ${basedir}/kali-$architecture/var/lib/alsa/
+cat << EOF > ${basedir}/kali-$architecture/var/lib/alsa/asound.state
 state.ROCKCHIPI2S {
     control.1 {
         iface MIXER
@@ -2170,7 +2151,7 @@ state.RockchipHDMI {
 }
 EOF
 
-cat << EOF > ${basedir}/root/etc/pulse/default.pa
+cat << EOF > ${basedir}/kali-$architecture/etc/pulse/default.pa
 #!/usr/bin/pulseaudio -nF
 #
 # This file is part of PulseAudio.
@@ -2337,14 +2318,13 @@ set-default-sink 0
 #set-default-source input
 EOF
 
-
 # mali rules so users can access the mali0 driver...
-cat << EOF > ${basedir}/root/etc/udev/rules.d/50-mali.rules
+cat << EOF > ${basedir}/kali-$architecture/etc/udev/rules.d/50-mali.rules
 KERNEL=="mali0", MODE="0660", GROUP="video"
 EOF
 
 # Video rules aka media-rules package in ChromeOS
-cat << EOF > ${basedir}/root/etc/udev/rules.d/50-media.rules
+cat << EOF > ${basedir}/kali-$architecture/etc/udev/rules.d/50-media.rules
 ATTR{name}=="s5p-mfc-dec", SYMLINK+="video-dec"
 ATTR{name}=="s5p-mfc-enc", SYMLINK+="video-enc"
 ATTR{name}=="s5p-jpeg-dec", SYMLINK+="jpeg-dec"
@@ -2362,8 +2342,8 @@ ATTR{name}=="mt81xx-image-proc", SYMLINK+="image-proc0"
 EOF
 
 # Touchpad configuration
-mkdir -p ${basedir}/root/etc/X11/xorg.conf.d
-cat << EOF > ${basedir}/root/etc/X11/xorg.conf.d/10-synaptics-chromebook.conf
+mkdir -p ${basedir}/kali-$architecture/etc/X11/xorg.conf.d
+cat << EOF > ${basedir}/kali-$architecture/etc/X11/xorg.conf.d/10-synaptics-chromebook.conf
 Section "InputClass"
 	Identifier		"touchpad"
 	MatchIsTouchpad		"on"
@@ -2377,27 +2357,52 @@ Section "InputClass"
 EndSection
 EOF
 
-cp ${basedir}/../misc/zram ${basedir}/root/etc/init.d/zram
-chmod 755 ${basedir}/root/etc/init.d/zram
+cp ${basedir}/../misc/zram ${basedir}/kali-$architecture/etc/init.d/zram
+chmod 755 ${basedir}/kali-$architecture/etc/init.d/zram
 
 # Copy the broadcom firmware files in (for now) - once sources are released,
 # will be able to do this without having a local copy.
-mkdir -p ${basedir}/root/lib/firmware/brcm/
-cp ${basedir}/../misc/brcm/* ${basedir}/root/lib/firmware/brcm/
+mkdir -p ${basedir}/kali-$architecture/lib/firmware/brcm/
+cp ${basedir}/../misc/brcm/* ${basedir}/kali-$architecture/lib/firmware/brcm/
 # Copy in the touchpad firmwares - same as above.
-cp ${basedir}/../misc/elan* ${basedir}/root/lib/firmware/
-cp ${basedir}/../misc/max* ${basedir}/root/lib/firmware/
+cp ${basedir}/../misc/elan* ${basedir}/kali-$architecture/lib/firmware/
+cp ${basedir}/../misc/max* ${basedir}/kali-$architecture/lib/firmware/
 cd ${basedir}
 
 # We need to kick start the sdio chip to get bluetooth/wifi going.  This is ugly
 # but bear with me.
-cp ${basedir}/../misc/bins/* ${basedir}/root/usr/sbin/
+cp ${basedir}/../misc/bins/* ${basedir}/kali-$architecture/usr/sbin/
 # And now we activate via udev rule
-cat << EOF > ${basedir}/root/etc/udev/rules.d/80-brcm-sdio-added.rules
+cat << EOF > ${basedir}/kali-$architecture/etc/udev/rules.d/80-brcm-sdio-added.rules
 ACTION=="add", SUBSYSTEM=="sdio", ENV{SDIO_CLASS}=="02", ENV{SDIO_ID}=="02D0:4354", RUN+="/usr/sbin/brcm_patchram_plus -d --patchram /lib/firmware/brcm/BCM4354_003.001.012.0306.0659.hcd --no2bytes --enable_hci --enable_lpm --scopcm=1,2,0,1,1,0,0,0,0,0 --baudrate 3000000 --use_baudrate_for_download --tosleep=50000 /dev/ttyS0"
 EOF
 
-sed -i -e 's/^#PermitRootLogin.*/PermitRootLogin yes/' ${basedir}/root/etc/ssh/sshd_config
+sed -i -e 's/^#PermitRootLogin.*/PermitRootLogin yes/' ${basedir}/kali-$architecture/etc/ssh/sshd_config
+
+echo "Creating image file for Veyron Chromebooks"
+dd if=/dev/zero of=${basedir}/$imagename.img bs=1M count=7000
+parted $imagename.img --script -- mklabel gpt
+cgpt create -z $imagename.img
+cgpt create $imagename.img
+
+cgpt add -i 1 -t kernel -b 8192 -s 32768 -l kernel -S 1 -T 5 -P 10 $imagename.img
+cgpt add -i 2 -t data -b 40960 -s `expr $(cgpt show $imagename.img | grep 'Sec GPT table' | awk '{ print \$1 }')  - 40960` -l Root $imagename.img
+
+loopdevice=`losetup -f --show ${basedir}/$imagename.img`
+device=`kpartx -va $loopdevice| sed -E 's/.*(loop[0-9])p.*/\1/g' | head -1`
+sleep 5
+device="/dev/mapper/${device}"
+bootp=${device}p1
+rootp=${device}p2
+
+mkfs.ext4 -O ^flex_bg -O ^metadata_csum -L rootfs $rootp
+
+mkdir -p ${basedir}/root
+mount $rootp ${basedir}/root
+
+echo "Rsyncing rootfs into image file"
+rsync -HPavz -q ${basedir}/kali-$architecture/ ${basedir}/root/
+sync
 
 # Unmount partitions
 umount $rootp
