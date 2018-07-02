d7000b70b9c8a9732d2846dc7b91348ba0fcf6c8
Steev Klimaszewski <steev@kali.org>
2018-07-02 10:16:16 -0500

    rpi3: Use the rpi3-firmware package instead of cloning git

--- rpi3-nexmon.sh
+++ rpi3-nexmon.sh
@@ -28,7 +28,7 @@ base="e2fsprogs initramfs-tools parted sudo usbutils firmware-linux firmware-rea
 desktop="fonts-croscore kali-defaults kali-menu fonts-crosextra-caladea fonts-crosextra-carlito gnome-theme-kali gtk3-engines-xfce kali-desktop-xfce kali-root-login lightdm network-manager network-manager-gnome xfce4 xserver-xorg-video-fbdev xserver-xorg-input-evdev xserver-xorg-input-synaptics"
 tools="aircrack-ng ethtool hydra john libnfc-bin mfoc nmap passing-the-hash sqlmap usbutils winexe wireshark"
 services="apache2 openssh-server"
-extras="iceweasel xfce4-terminal wpasupplicant python-smbus i2c-tools python-requests python-configobj python-pip bluez bluez-firmware"
+extras="iceweasel xfce4-terminal wpasupplicant python-smbus i2c-tools python-requests python-configobj python-pip bluez bluez-firmware rpi3-firmware"
 nexmon="libgmp3-dev gawk qpdf bison flex make git"
 
 # Git commit hash to check out for the kernel
@@ -361,9 +361,6 @@ echo "T0:23:respawn:/sbin/agetty -L ttyAMA0 115200 vt100" >> ${basedir}/kali-${a
 
 # Kernel section. If you want to use a custom kernel, or configuration, replace
 # them in this section.
-git clone --depth 1 https://github.com/raspberrypi/firmware.git rpi-firmware
-cp -rf rpi-firmware/boot/* ${basedir}/kali-${architecture}/boot/
-rm -rf rpi-firmware
 git clone --depth 1 https://github.com/nethunteros/re4son-raspberrypi-linux.git -b rpi-4.9.80-re4son ${basedir}/kali-${architecture}/usr/src/kernel
 cd ${basedir}/kali-${architecture}/usr/src/kernel
 export ARCH=arm
@@ -395,7 +392,7 @@ ln -s /usr/src/kernel source
 cd ${basedir}
 
 # Create cmdline.txt file
-cd ${basedir}  
+cd ${basedir}
 
 cat << EOF > ${basedir}/kali-${architecture}/boot/cmdline.txt
 dwc_otg.fiq_fix_enable=2 console=ttyAMA0,115200 kgdboc=ttyAMA0,115200 console=tty1 root=/dev/mmcblk0p2 rootfstype=ext4 rootwait rootflags=noload net.ifnames=0
