a598bfeefe8cb0fd05cb65d5257aa7a952ece141
Steev Klimaszewski <steev@kali.org>
2018-07-02 13:14:14 -0500

    build-deps: Moar waiting.
    When building 2 images close together, we can race with the dpkg lock
    so let's slap more waits in to work around it.

--- build-deps.sh
+++ build-deps.sh
@@ -10,6 +10,12 @@ while fuser /var/lib/dpkg/lock >/dev/null ; do
 done
 
 apt-get update
+
+# We can get a race when building 2 images around the same time.
+while fuser /var/lib/dpkg/lock >/dev/null ; do
+    sleep 5
+done
+
 apt-get install -y git-core gnupg flex bison gperf build-essential \
 zip curl libncurses5-dev zlib1g-dev libncurses5-dev kali-archive-keyring \
 parted kpartx debootstrap pixz qemu-user-static abootimg cgpt vboot-kernel-utils \
@@ -22,10 +28,19 @@ systemd-container
 echo "Waiting for other software manager to finish..."
 MACHINE_TYPE=`uname -m`
 if [ ${MACHINE_TYPE} == 'x86_64' ]; then
+    while fuser /var/lib/dpkg/lock >/dev/null ; do
+        sleep 5
+    done
     dpkg --add-architecture i386
     # apt-get update to make sure we have a list of the i386 packages
     apt-get update
+    while fuser /var/lib/dpkg/lock >/dev/null ; do
+        sleep 5
+    done
     apt-get install -y libstdc++6:i386 libgcc1:i386 zlib1g:i386 libncurses5:i386
 else
+    while fuser /var/lib/dpkg/lock >/dev/null ; do
+        sleep 5
+    done
     apt-get install -y libncurses5
 fi
