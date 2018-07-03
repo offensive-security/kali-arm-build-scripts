55c6e703c5429328a1199786b849d9053d61d8f9
Steev Klimaszewski <steev@kali.org>
2018-07-03 18:40:39 -0500

    usbarmory: Remove unimplemented die function

--- usbarmory.sh
+++ usbarmory.sh
@@ -133,12 +133,12 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get --yes --allow-change-held-packages install ${packages}
 if [[ $? > 0 ]];
 then
-    apt-get --yes --allow-change-held-packages --fix-broken install || die "Packages failed to install"
+    apt-get --yes --allow-change-held-packages --fix-broken install
 fi
 apt-get --yes --allow-change-held-packages install ${desktop} ${tools}
 if [[ $? > 0 ]];
 then
-    apt-get --yes --allow-change-held-packages --fix-broken install || die "Packages failed to install"
+    apt-get --yes --allow-change-held-packages --fix-broken install
 fi
 apt-get --yes --allow-change-held-packages dist-upgrade
 apt-get --yes --allow-change-held-packages autoremove
