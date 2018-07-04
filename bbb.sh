fed0c917a41e040740ef973c4e367aabe2b271a2
Steev Klimaszewski <steev@kali.org>
2018-07-04 04:47:06 -0500

    bbb: $0 is the script, $1 is the version number.

--- bbb.sh
+++ bbb.sh
@@ -63,7 +63,7 @@ cp /usr/bin/qemu-arm-static kali-${architecture}/usr/bin/
 
 LANG=C systemd-nspawn -M $machine -D kali-${architecture} /debootstrap/debootstrap --second-stage
 
-if [ $0 == 'nightly' ]; then
+if [ $1 == 'nightly' ]; then
 cat << EOF > kali-${architecture}/etc/apt/sources.list
 deb http://${mirror}/kali kali-rolling main contrib non-free
 EOF
