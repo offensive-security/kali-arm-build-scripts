18e94eeec56dd5ff193e787164c403e0888d0a96
Steev Klimaszewski <steev@kali.org>
2018-07-04 13:01:37 -0500

    rpitft: force bomb so that i can test exiting chrt

--- rpi-tft.sh
+++ rpi-tft.sh
@@ -190,6 +190,10 @@ rm -f /usr/sbin/invoke-rc.d
 dpkg-divert --remove --rename /usr/sbin/invoke-rc.d
 
 rm -f /third-stage
+if [[ $? = 1 ]]; then
+  echo "causing the container to die on purpose"
+  systemctl exit 1
+fi
 EOF
 
 chmod 755 kali-${architecture}/third-stage
