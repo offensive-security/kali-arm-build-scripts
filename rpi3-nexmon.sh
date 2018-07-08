02909362f2013907c663d84c8fdd5b267d57269d
Steev Klimaszewski <steev@kali.org>
2018-07-07 21:44:01 -0500

    rpi3: Let's rm nexmon IN the image, not the host system, and also lets also install apt-utils

--- rpi3-nexmon.sh
+++ rpi3-nexmon.sh
@@ -30,7 +30,7 @@ suite=kali-rolling
 machine=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 16 | head -n 1)
 
 arm="abootimg cgpt fake-hwclock ntpdate u-boot-tools vboot-utils vboot-kernel-utils"
-base="kali-defaults e2fsprogs ifupdown initramfs-tools parted sudo usbutils firmware-linux firmware-realtek firmware-atheros firmware-libertas firmware-brcm80211 net-tools iw wget"
+base="apt-utils kali-defaults e2fsprogs ifupdown initramfs-tools parted sudo usbutils firmware-linux firmware-realtek firmware-atheros firmware-libertas firmware-brcm80211 net-tools iw wget"
 desktop="kali-menu fonts-croscore kali-defaults kali-menu fonts-crosextra-caladea fonts-crosextra-carlito gnome-theme-kali gtk3-engines-xfce kali-desktop-xfce kali-root-login lightdm network-manager network-manager-gnome xfce4 xserver-xorg-video-fbdev xserver-xorg-input-evdev xserver-xorg-input-synaptics"
 tools="aircrack-ng ethtool hydra john libnfc-bin mfoc nmap passing-the-hash sqlmap usbutils winexe wireshark"
 services="apache2 openssh-server"
@@ -297,9 +297,10 @@ cp ${NEXMON_ROOT}/firmwares/bcm43430a1/7_45_41_46/brcmfmac43430-sdio.bin /lib/fi
 # RPI3B+ is bcm43455c0 (version 7.45.154)
 cp ${NEXMON_ROOT}/firmwares/bcm43455c0/7_45_154/brcmfmac43455-sdio.bin /lib/firmware/brcm/brcmfmac43455-sdio.nexmon.bin
 cp ${NEXMON_ROOT}/firmwares/bcm43455c0/7_45_154/brcmfmac43455-sdio.bin /lib/firmware/brcm/brcmfmac43455-sdio.bin
-EOF
+
 # And now remove the nexmon sources because that's 2.5GB of space we don't want to give up.
 rm -rf /opt/nexmon
+EOF
 chmod 755 ${basedir}/kali-${architecture}/root/buildnexmonfirmware.sh
 
 
