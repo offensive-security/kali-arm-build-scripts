12a84c0f362a524a9879e9b6fcd7459b51ba60a4
Steev Klimaszewski <steev@kali.org>
2018-07-09 22:18:11 -0500

    0w: Install firmware-brcm

--- rpi0w-nexmon.sh
+++ rpi0w-nexmon.sh
@@ -44,7 +44,7 @@ machine=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 16 | head -n 1)
 # image, keep that in mind.
 
 arm="abootimg cgpt fake-hwclock ntpdate vboot-utils vboot-kernel-utils u-boot-tools"
-base="apt-utils kali-defaults ifupdown initramfs-tools sudo parted e2fsprogs usbutils firmware-linux firmware-realtek firmware-libertas firmware-atheros"
+base="apt-utils kali-defaults ifupdown initramfs-tools sudo parted e2fsprogs usbutils firmware-linux firmware-realtek firmware-libertas firmware-atheros firmware-brcm80211"
 #desktop="fonts-croscore fonts-crosextra-caladea fonts-crosextra-carlito gnome-theme-kali gtk3-engines-xfce kali-desktop-xfce kali-root-login lightdm network-manager network-manager-gnome xfce4 xserver-xorg-video-fbdev xserver-xorg-input-evdev xserver-xorg-input-synaptics"
 tools="kali-menu passing-the-hash winexe aircrack-ng hydra john sqlmap libnfc-bin mfoc nmap ethtool usbutils net-tools"
 services="openssh-server apache2"
