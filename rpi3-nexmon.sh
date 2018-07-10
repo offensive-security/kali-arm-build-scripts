01dcb07defabcbdbc40e35cbb04ab5ca760adc32
Steev Klimaszewski <steev@kali.org>
2018-07-09 21:04:06 -0500

    rpi3: Define CC and ARM on the make lines for the firmwares

--- rpi3-nexmon.sh
+++ rpi3-nexmon.sh
@@ -443,13 +443,13 @@ cd ${NEXMON_ROOT}/patches/bcm43430a1/7_45_41_46/nexmon
 # Make sure we use the cross compiler to build the firmware.
 # We use the x86 cross compiler because we're building on amd64
 unset CROSS_COMPILE
-export CROSS_COMPILE=${NEXMON_ROOT}/buildtools/gcc-arm-none-eabi-5_4-2016q2-linux-x86/bin/arm-none-eabi-
+#export CROSS_COMPILE=${NEXMON_ROOT}/buildtools/gcc-arm-none-eabi-5_4-2016q2-linux-x86/bin/arm-none-eabi-
 make clean
 # We do this so we don't have to install the ancient isl version into /usr/local/lib on systems.
-LD_LIBRARY_PATH=${NEXMON_ROOT}/buildtools/isl-0.10/.libs make
+LD_LIBRARY_PATH=${NEXMON_ROOT}/buildtools/isl-0.10/.libs make ARCH=arm CC=${NEXMON_ROOT}/buildtools/gcc-arm-none-eabi-5_4-2016q2-linux-x86/bin/arm-none-eabi-
 cd ${NEXMON_ROOT}/patches/bcm43455c0/7_45_154/nexmon
 make clean
-LD_LIBRARY_PATH=${NEXMON_ROOT}/buildtools/isl-0.10/.libs make
+LD_LIBRARY_PATH=${NEXMON_ROOT}/buildtools/isl-0.10/.libs make ARCH=arm CC=${NEXMON_ROOT}/buildtools/gcc-arm-none-eabi-5_4-2016q2-linux-x86/bin/arm-none-eabi-
 # RPi0w->3B firmware
 cp ${NEXMON_ROOT}/patches/bcm43430a1/7_45_41_46/nexmon/brcmfmac43430-sdio.bin ${basedir}/kali-${architecture}/lib/firmware/brcm/brcmfmac43430-sdio.nexmon.bin
 cp ${NEXMON_ROOT}/patches/bcm43430a1/7_45_41_46/nexmon/brcmfmac43430-sdio.bin ${basedir}/kali-${architecture}/lib/firmware/brcm/brcmfmac43430-sdio.bin
