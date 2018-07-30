5f3515162d119f0ce3d0ce47dedb64b79792c5b4
Steev Klimaszewski <steev@kali.org>
2018-07-30 18:53:04 -0500

    odroidxu: Old cross compiler for old kernel

--- odroid-xu.sh
+++ odroid-xu.sh
@@ -236,6 +236,10 @@ EOF
 # Uncomment this if you use apt-cacher-ng otherwise git clones will fail.
 #unset http_proxy
 
+# We need an older gcc because of kernel age.
+cd "${basedir}"
+git clone --depth 1 https://github.com/offensive-security/gcc-arm-linux-gnueabihf-4.7
+
 # Kernel section. If you want to use a custom kernel, or configuration, replace
 # them in this section.
 git clone --depth 1 https://github.com/hardkernel/linux.git -b odroidxu-3.4.y "${basedir}"/kali-${architecture}/usr/src/kernel
@@ -244,7 +248,7 @@ git rev-parse HEAD > "${basedir}"/kali-${architecture}/usr/src/kernel-at-commit
 patch -p1 --no-backup-if-mismatch < "${basedir}"/../patches/mac80211.patch
 touch .scmversion
 export ARCH=arm
-export CROSS_COMPILE=arm-linux-gnueabihf-
+export CROSS_COMPILE="${basedir}"/gcc-arm-linux-gnueabihf-4.7/bin/arm-linux-gnueabihf-
 cp "${basedir}"/../kernel-configs/xu.config .config
 cp "${basedir}"/../kernel-configs/xu.config "${basedir}"/kali-${architecture}/usr/src/xu.config
 make -j $(grep -c processor /proc/cpuinfo)
