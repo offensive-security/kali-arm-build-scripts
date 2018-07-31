041607620828ec949f24307463cd4aff8ddcf113
Steev Klimaszewski <steev@kali.org>
2018-07-30 19:23:39 -0500

    xu: Create rc.local so we can get console output, maybe

--- odroid-xu.sh
+++ odroid-xu.sh
@@ -267,6 +267,22 @@ make
 make DESTDIR="${basedir}"/kali-${architecture} install
 
 cd "${basedir}"/kali-${architecture}/etc/
+cat << EOF > "${basedir}"/kali-${architecture}/etc/rc.local
+#!/bin/sh -e
+#
+# rc.local
+#
+# This script is executed at the end of each multiuser runlevel.
+# Make sure that the script will "exit 0" on success or any other
+# value on error.
+#
+# In order to enable or disable this script just change the execution
+# bits.
+#
+# By default this script does nothing.
+
+exit 0
+EOF
 sed -i -e 's~^exit 0~exynos5-hwcomposer > /dev/null 2>\&1 \&\nexit 0~' rc.local
 
 cd "${basedir}"
