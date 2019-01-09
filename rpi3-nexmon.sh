fa46deb78d34e8470714818196016088167169ca
Steev Klimaszewski <steev@kali.org>
2019-01-09 07:19:59 +0000

    rpi3-nexmon: Move to using re4son's repository
    This lets us pull in the kernel headers, boot loader, and kernel itself.
    
    Pushing this out to get it testing on the builder.

--- rpi3-nexmon.sh
+++ rpi3-nexmon.sh
@@ -26,12 +26,6 @@ size=5000
 # kali-rolling.
 suite=kali-rolling
 
-# Set kernel version here, to make it faster to upgrade to newer versions.
-# Version number comes from https://github.com/nethunteros/rpi-kernel/releases
-# All we need are the numbers.
-kernrelease=v4.14.80
-rpikernelver=20181124-191322
-
 # Generate a random machine name to be used.
 machine=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 16 | head -n 1)
 
@@ -40,7 +34,7 @@ base="apt-transport-https apt-utils console-setup e2fsprogs firmware-linux firmw
 desktop="kali-menu fonts-croscore fonts-crosextra-caladea fonts-crosextra-carlito gtk3-engines-xfce kali-desktop-xfce kali-root-login lightdm network-manager network-manager-gnome xfce4 xserver-xorg-video-fbdev xserver-xorg-input-evdev xserver-xorg-input-synaptics"
 tools="aircrack-ng crunch cewl dnsrecon dnsutils ethtool exploitdb hydra john libnfc-bin medusa metasploit-framework mfoc ncrack nmap passing-the-hash proxychains recon-ng sqlmap tcpdump theharvester tor tshark usbutils whois windows-binaries winexe wpscan wireshark"
 services="apache2 atftpd openssh-server openvpn tightvncserver"
-extras="bluez bluez-firmware firefox-esr i2c-tools python-configobj python-pip python-requests python-rpi.gpio python-smbus raspi3-firmware triggerhappy wpasupplicant xfce4-terminal xfonts-terminus"
+extras="bluez bluez-firmware firefox-esr i2c-tools python-configobj python-pip python-requests python-rpi.gpio python-smbus triggerhappy wpasupplicant xfce4-terminal xfonts-terminus"
 
 packages="${arm} ${base} ${services} ${extras}"
 
@@ -200,12 +194,6 @@ cp "${basedir}"/../misc/pi-bluetooth/btuart "${basedir}"/kali-${architecture}/us
 # Ensure btuart is executable
 chmod 755 "${basedir}"/kali-${architecture}/usr/bin/btuart
 
-# Let's try out binky's package for the rpi kernel and headers.
-wget https://github.com/nethunteros/rpi-kernel/releases/download/${kernrelease}-re4son/raspberrypi-kernel_${rpikernelver}_armhf.deb -O "${basedir}"/kali-${architecture}/root/raspberrypi-kernel_${rpikernelver}_armhf.deb
-wget https://github.com/nethunteros/rpi-kernel/releases/download/${kernrelease}-re4son/raspberrypi-kernel-headers_${rpikernelver}_armhf.deb -O "${basedir}"/kali-${architecture}/root/raspberrypi-kernel-headers_${rpikernelver}_armhf.deb
-# Mister-X's libfakeioctl fixes
-#cp "${basedir}"/../misc/fakeioctl.c "${basedir}"/kali-${architecture}/root/fakeioctl.c
-
 cat << EOF > "${basedir}"/kali-${architecture}/third-stage
 #!/bin/bash
 set -e
@@ -229,7 +217,10 @@ apt-get --yes --allow-change-held-packages install ${desktop} ${tools} || apt-ge
 apt-get --yes --allow-change-held-packages install ${desktop} ${tools} || apt-get --yes --fix-broken install
 
 # Install the kernel packages
-dpkg -i /root/raspberrypi-kernel_${rpikernelver}_armhf.deb /root/raspberrypi-kernel-headers_${rpikernelver}_armhf.deb
+echo "deb http://http.re4son-kernel.com/re4son kali-pi main" > /etc/apt/sources.list.d/re4son.list
+wget -O - https://re4son-kernel.com/keys/http/archive-key.asc | apt-key add -
+apt-get update
+apt-get install --yes --allow-change-held-packages kalipi-kernel kalipi-bootloader kalipi-re4son-firmware kalipi-kernel-headers
 
 apt-get --yes --allow-change-held-packages autoremove
 # libinput seems to fail hard on RaspberryPi devices, so we make sure it's not
@@ -328,8 +319,6 @@ cat << EOF > "${basedir}"/kali-${architecture}/etc/fstab
 proc            /proc           proc    defaults          0       0
 /dev/mmcblk0p1  /boot           vfat    defaults          0       2
 /dev/mmcblk0p2  /               ext4    defaults,noatime  0       1
-# This is a bit of a hack because of using Debian's raspi3-firmware package.
-tmpfs     /boot/firmware  tmpfs rw            0       0
 EOF
 
 # Copy a default config, with everything commented out so people find it when
@@ -346,68 +335,17 @@ cat << EOF >> "${basedir}"/kali-${architecture}/boot/config.txt
 #program_usb_boot_mode=1
 EOF
 
-# Because we use debian's firmware package and they install it to /boot/firmware instead of /boot directly
-# we have to mv it to /boot so the thing will boot.
-mv "${basedir}"/kali-${architecture}/boot/firmware/* "${basedir}"/kali-${architecture}/boot/
-
 cp "${basedir}"/../misc/zram "${basedir}"/kali-${architecture}/etc/init.d/zram
 chmod 755 "${basedir}"/kali-${architecture}/etc/init.d/zram
 
 # Set a REGDOMAIN.  This needs to be done or wireless doesn't work correctly on the RPi 3B+
 sed -i -e 's/REGDOM.*/REGDOMAIN=00/g' "${basedir}"/kali-${architecture}/etc/default/crda
 
-# Build nexmon firmware outside the build system, if we can.
-cd "${basedir}"
-git clone https://github.com/seemoo-lab/nexmon.git "${basedir}"/nexmon --depth 1
-cd "${basedir}"/nexmon
-# Disable statistics
-touch DISABLE_STATISTICS
-source setup_env.sh
-ls -lah /usr/lib/x86_64-linux-gnu/libl.a
-ls -lah /usr/lib/x86_64-linux-gnu/libfl.a
-make
-cd buildtools/isl-0.10
-CC=$CCgcc
-./configure
-make
-sed -i -e 's/all:.*/all: $(RAM_FILE)/g' ${NEXMON_ROOT}/patches/bcm43430a1/7_45_41_46/nexmon/Makefile
-sed -i -e 's/all:.*/all: $(RAM_FILE)/g' ${NEXMON_ROOT}/patches/bcm43455c0/7_45_154/nexmon/Makefile
-cd ${NEXMON_ROOT}/patches/bcm43430a1/7_45_41_46/nexmon
-# Make sure we use the cross compiler to build the firmware.
-# We use the x86 cross compiler because we're building on amd64
-unset CROSS_COMPILE
-#export CROSS_COMPILE=${NEXMON_ROOT}/buildtools/gcc-arm-none-eabi-5_4-2016q2-linux-x86/bin/arm-none-eabi-
-make clean
-# We do this so we don't have to install the ancient isl version into /usr/local/lib on systems.
-LD_LIBRARY_PATH=${NEXMON_ROOT}/buildtools/isl-0.10/.libs make ARCH=arm CC=${NEXMON_ROOT}/buildtools/gcc-arm-none-eabi-5_4-2016q2-linux-x86/bin/arm-none-eabi-
-cd ${NEXMON_ROOT}/patches/bcm43455c0/7_45_154/nexmon
-make clean
-LD_LIBRARY_PATH=${NEXMON_ROOT}/buildtools/isl-0.10/.libs make ARCH=arm CC=${NEXMON_ROOT}/buildtools/gcc-arm-none-eabi-5_4-2016q2-linux-x86/bin/arm-none-eabi-
-# RPi0w->3B firmware
-mkdir -p "${basedir}"/kali-${architecture}/lib/firmware/brcm
-cp ${NEXMON_ROOT}/patches/bcm43430a1/7_45_41_46/nexmon/brcmfmac43430-sdio.bin "${basedir}"/kali-${architecture}/lib/firmware/brcm/brcmfmac43430-sdio.nexmon.bin
-cp ${NEXMON_ROOT}/patches/bcm43430a1/7_45_41_46/nexmon/brcmfmac43430-sdio.bin "${basedir}"/kali-${architecture}/lib/firmware/brcm/brcmfmac43430-sdio.bin
-wget https://raw.githubusercontent.com/RPi-Distro/firmware-nonfree/master/brcm/brcmfmac43430-sdio.txt -O "${basedir}"/kali-${architecture}/lib/firmware/brcm/brcmfmac43430-sdio.txt
-# RPi3B+ firmware
-cp ${NEXMON_ROOT}/patches/bcm43455c0/7_45_154/nexmon/brcmfmac43455-sdio.bin "${basedir}"/kali-${architecture}/lib/firmware/brcm/brcmfmac43455-sdio.nexmon.bin
-cp ${NEXMON_ROOT}/patches/bcm43455c0/7_45_154/nexmon/brcmfmac43455-sdio.bin "${basedir}"/kali-${architecture}/lib/firmware/brcm/brcmfmac43455-sdio.bin
-wget https://raw.githubusercontent.com/RPi-Distro/firmware-nonfree/master/brcm/brcmfmac43455-sdio.txt -O "${basedir}"/kali-${architecture}/lib/firmware/brcm/brcmfmac43455-sdio.txt
-# Make a backup copy of the rpi firmware in case people don't want to use the nexmon firmware.
-# The firmware used on the RPi is not the same firmware that is in the firmware-brcm package which is why we do this.
-wget https://raw.githubusercontent.com/RPi-Distro/firmware-nonfree/master/brcm/brcmfmac43430-sdio.bin -O "${basedir}"/kali-${architecture}/lib/firmware/brcm/brcmfmac43430-sdio.rpi.bin
-wget https://raw.githubusercontent.com/RPi-Distro/firmware-nonfree/master/brcm/brcmfmac43455-sdio.bin -O "${basedir}"/kali-${architecture}/lib/firmware/brcm/brcmfmac43455-sdio.rpi.bin
-# This is required for any wifi to work on the RPi 3B+
-wget https://raw.githubusercontent.com/RPi-Distro/firmware-nonfree/master/brcm/brcmfmac43455-sdio.clm_blob -O "${basedir}"/kali-${architecture}/lib/firmware/brcm/brcmfmac43455-sdio.clm_blob
-
-rm -rf "${basedir}"/kali-${architecture}/root/{fakeuname.c,buildnexmon.sh,libfakeuname.so,raspberrypi-kernel*.deb}
-
 cd "${basedir}"
 
 # Re4son's rpi-tft configurator
 wget https://raw.githubusercontent.com/Re4son/RPi-Tweaks/master/kalipi-tft-config/kalipi-tft-config -O "${basedir}"/kali-${architecture}/usr/bin/kalipi-tft-config
 chmod 755 "${basedir}"/kali-${architecture}/usr/bin/kalipi-tft-config
-
-rm -rf "${basedir}"/kali-${architecture}/root/{fakeuname.c,buildnexmon.sh,libfakeuname.so,raspberrypi-kernel*.deb}
 
 echo "Running du to see how big kali-${architecture} is"
 du -sh "${basedir}"/kali-${architecture}
