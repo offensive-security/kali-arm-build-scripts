437640f1cc9f0c394ad76f87ceb0117c88038926
Steev Klimaszewski <steev@kali.org>
2019-02-11 22:07:26 -0600

    rpi3: Fix the shared-mime-info hack

--- rpi3-nexmon.sh
+++ rpi3-nexmon.sh
@@ -130,7 +130,7 @@ Description=shared-mime-info update hack
 Before=regenerate_ssh_host_keys.service
 [Service]
 Type=oneshot
-ExecStart=/usr/bin/update-mime-database  /usr/share/mime/
+ExecStart=/bin/sh -c "dpkg-reconfigure shared-mime-info"
 ExecStartPost=/bin/systemctl disable smi-hack
 
 [Install]
